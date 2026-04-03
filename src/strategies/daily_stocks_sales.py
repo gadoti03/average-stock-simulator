51c20fd83089d175d99794ffc8e9ab6a22e57c27
Gabriele <dotigabriele@gmail.com>
2026-04-03 21:34:39 +0200

    updated strategy

--- src/strategies/daily_stocks_sales.py
+++ src/strategies/daily_stocks_sales.py
@@ -3,8 +3,8 @@ from .base_strategy import AnalysisStrategy
 class DailyStockStrategy(AnalysisStrategy):
     def compute(self, result, warehouse, total_days):
         filtered_stocks = [
-            stock for stock, sales in zip(result.daily_stocks, result.daily_sales)
-            if not (stock == 0 and sales == 0)
+            stock for stock, exits in zip(result.daily_stocks, result.daily_exits)
+            if not (stock == 0 and exits == 0)
         ]
 
         avg = sum(filtered_stocks) / len(filtered_stocks) if filtered_stocks else 0
